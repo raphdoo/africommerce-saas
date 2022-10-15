6b08e25f834b76f6c1805612207c6a0a4e7f6ca3
Rapheal Ajiboye <raphooo95@gmail.com>
2022-10-15 10:08:22 +0100

    fix: issue 5, user model

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var config = require('./config/config')
 
 var { connectToDatabase } = require("./db")
 
